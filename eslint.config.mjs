62d9b64d5ce6964c20dca326f2a47821709e7e9c
Accept77 <accept_agreed@kakao.com>
2025-05-15 13:10:52 +0900

    chore: husky precommit prepush fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,23 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  extends: ["react-app", "react-app/jest", "plugin:prettier/recommended", "plugin:tailwindcss/recommended","plugin:storybook/recommended","plugin:react-hooks/recommended",],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "react-hooks"],
+  rules: {
+    "no-unused-vars": "off",
+    "no-undef": "off",
+    "tailwindcss/no-custom-classname": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "no-console": ["error", { allow: ["warn", "error", "info"] }],
+    "eqeqeq": "error",
+    "no-var": "error",
+  },
 });
 
 const eslintConfig = [
