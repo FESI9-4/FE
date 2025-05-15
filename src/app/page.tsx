bddb6ce032ca618cee635c38dd8cc1a9a2b819b8
Accept77 <accept_agreed@kakao.com>
2025-05-15 15:15:35 +0900

    fix: page.tsx fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import Tap from '@/components/tap';
-
 export default function Home() {
-    return (
-        <div className="flex gap-2 p-10">
-            <Tap status="active" size="large">
-                전체
-            </Tap>
-            <Tap status="active" size="small">
-                전체
-            </Tap>
-            <Tap status="default" size="large">
-                전체
-            </Tap>
-            <Tap status="default" size="small">
-                전체
-            </Tap>
-        </div>
-    );
+    return <div className="flex gap-2 p-10"></div>;
 }
