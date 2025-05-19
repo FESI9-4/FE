5a9c1b7c4bee88020a3ae3c4511480bef251673f
GitHub Oaelan <1djb23@naver.com>
2025-05-19 20:50:08 +0900

    refactor: 비밀번호 숨김 버튼에 포인터 추가와 svg 태그 대신 Image로 교체

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -20,6 +20,4 @@ export {
 };
 import Input from './Input';
 import InputQnA from './InputQnA';
-import BoxSelect from './BoxSelect';
-import BoxSelectGroup from './BoxSelectGroup';
-export { Input, InputQnA, BoxSelect, BoxSelectGroup };
+export { Input, InputQnA };
