20567e2872545a01cd840bba01bbf86716614796
GitHub Oaelan <1djb23@naver.com>
2025-05-19 13:00:08 +0900

    feature: 공용 인풋 컴포넌트 제작

---
+++ src/components/ui/index.ts
@@ -0,0 +1,5 @@
+import Input from './Input';
+import InputQnA from './InputQnA';
+import BoxSelect from './BoxSelect';
+import BoxSelectGroup from './BoxSelectGroup';
+export { Input, InputQnA, BoxSelect, BoxSelectGroup };
