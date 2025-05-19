ae9caf685826d3315ee12be30a64bccaac3907dc
jaehyeongjung <titochung625@naver.com>
2025-05-19 15:36:15 +0900

    refactor: min-w-85.5 ->  찜한팬팔추가시 더 낮아지면 ui 잘림

---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,9 @@
+import Nav from '@/components/ui/Nav';
+
+export default function navTest() {
+    return (
+        <div className="text-3xl font-bold">
+            <Nav></Nav>
+        </div>
+    );
+}
