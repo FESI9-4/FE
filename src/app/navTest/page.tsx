1a4298bbeca334add4211ee02cfc69e12f7542aa
jaehyeongjung <titochung625@naver.com>
2025-05-19 16:31:38 +0900

    feat: nav 찜하기 count props 받기

--- src/app/navTest/page.tsx
+++ src/app/navTest/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+import { useState } from 'react';
 import Nav from '@/components/ui/Nav';
 
-export default function navTest() {
+export default function NavTest() {
+    const [wishlistCount, setWishlistCount] = useState(0);
+
+    const handleAddToWishlist = () => {
+        setWishlistCount((prev) => prev + 1);
+    };
+
     return (
-        <div className="text-3xl font-bold">
-            <Nav></Nav>
+        <div className="text-3xl font-bold flex flex-col justify-center items-center gap-10">
+            <Nav wishlistCount={wishlistCount} />
+            <button
+                className="w-30 h-10 bg-orange-300 text-white rounded"
+                onClick={handleAddToWishlist}
+            >
+                찜하기 +1
+            </button>
         </div>
     );
 }
