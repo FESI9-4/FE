e7f84cee2beaa6700d5fb99c17e5f2eca504ce6d
GitHub Oaelan <1djb23@naver.com>
2025-05-19 13:04:31 +0900

    feature:Input컴포넌트에서 사용되는 useInputState 훅과 폼 상태 관리를 위한 useForm훅 추가

---
+++ src/hooks/useInputState.ts
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+
+type InputState = 'default' | 'hover' | 'typing' | 'done' | 'error';
+
+export default function useInputState() {
+    // 패스워드 타입일때 눈모양 버튼을 통해 관리되는 비밀번호 숨기기 보이기 상태
+    const [isVisible, setIsVisible] = useState(false);
+    // 인풋 상태
+    const [inputState, setInputState] = useState<InputState>('default');
+    // 패스워드 버튼 클릭 이벤트
+    function handleVisibleClick() {
+        setIsVisible(!isVisible);
+    }
+    // 넘버 타입 +,- 버튼 클릭 이벤트
+    function handleAddClick(value: string, setCount: (count: string) => void) {
+        const newValue = Number(value) + 1;
+        if (newValue >= 999) setCount?.(String(999));
+        else setCount?.(String(newValue));
+    }
+    function handleSubClick(value: string, setCount: (count: string) => void) {
+        const newValue = Number(value) - 1;
+        if (newValue < 0) setCount?.(String(0));
+        else setCount?.(String(newValue));
+    }
+    // 호버 이벤트
+    function handleHover() {
+        setInputState('hover');
+    }
+    // 마우스 오버 이벤트
+    function handleLeave(isValid: boolean, value: string) {
+        if (!isValid) setInputState('error');
+        else if (value.length > 0) setInputState('done');
+        else setInputState('default');
+    }
+    // 블러 이벤트
+    function handleBlur(
+        e: React.FocusEvent<HTMLInputElement>,
+        isValid: boolean,
+        value: string,
+        onBlur: (e: React.FocusEvent<HTMLInputElement>) => void
+    ) {
+        onBlur?.(e);
+        if (!isValid) setInputState('error');
+        else if (isValid && value.length > 0) setInputState('done');
+    }
+    //체인지 이벤트
+    function handleChange(
+        e: React.ChangeEvent<HTMLInputElement>,
+        type: string,
+        setCount: (count: string) => void,
+        onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    ) {
+        // 넘버 타입일 경우 최대 최소 값 제한
+        if (type === 'number') {
+            if (Number(e.target.value) >= 999) setCount?.(String(999));
+            else if (Number(e.target.value) < 0) setCount?.(String(0));
+            else setCount?.(e.target.value);
+        }
+        setInputState('typing');
+        onChange?.(e); // 값 변경은 상위에 위임
+    }
+    return {
+        isVisible,
+        inputState,
+        setInputState,
+        handleVisibleClick,
+        handleAddClick,
+        handleSubClick,
+        handleHover,
+        handleLeave,
+        handleBlur,
+        handleChange,
+    };
+}
