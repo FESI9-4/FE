d9d81312856418d22bad5da290dc28a7abd0b948
Accept77 <accept_agreed@kakao.com>
2025-05-19 18:07:40 +0900

    feat: tab animation done

--- src/components/ui/tab.tsx
+++ src/components/ui/tab.tsx
@@ -1,43 +1,50 @@
 import clsx from 'clsx';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 interface TabProps {
     children: React.ReactNode;
-    icon?: 'do' | 'go';
-    active?: boolean;
+    icon: 'do' | 'go';
+    active: boolean;
     onClick?: () => void;
 }
 
-export default function Tab({
-    children,
-    icon = 'do',
-    active = false,
-    onClick,
-}: TabProps) {
-    const className = clsx('flex items-center gap-1 font-semibold text-lg', {
-        'text-gray-900': active,
-        'text-gray-400': !active,
-    });
-    const iconClassName = active
-        ? ''
-        : 'invert-[70%] sepia-[5%] saturate-[578%] hue-rotate-[179deg] brightness-[92%] contrast-[92%]';
+export default function Tab({ children, icon, active, onClick }: TabProps) {
+    const className = clsx(
+        'flex items-center gap-1 font-semibold text-lg hover:cursor-pointer'
+    );
 
     return (
-        <div
-            className="flex items-center gap-1 flex-col hover:cursor-pointer"
+        <motion.div
+            className={className}
+            animate={{
+                color: active ? '#111827' : '#9CA3AF', // gray-900 : gray-400
+            }}
+            transition={{
+                duration: 0.5,
+                ease: 'easeInOut',
+            }}
             onClick={onClick}
         >
-            <div className={className}>
-                {children}
+            {children}
+            <motion.div
+                animate={{
+                    filter: active
+                        ? 'none'
+                        : 'invert(70%) sepia(5%) saturate(578%) hue-rotate(179deg) brightness(92%) contrast(92%)',
+                }}
+                transition={{
+                    duration: 0.5,
+                    ease: 'easeInOut',
+                }}
+            >
                 <Image
                     src={`/icons/${icon}.svg`}
                     alt={icon}
                     width={24}
                     height={24}
-                    className={iconClassName}
                 />
-            </div>
-            {active && <hr className="w-full h-0.5 bg-gray-900 " />}
-        </div>
+            </motion.div>
+        </motion.div>
     );
 }
