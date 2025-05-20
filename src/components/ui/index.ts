f7bd261a9e09a747c04db8067e35b180b81023c0
Accept77 <accept_agreed@kakao.com>
2025-05-20 17:59:15 +0900

    feat: cardlist working

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,16 +1,16 @@
+import ChipInfo from './ChipInfo';
 import ChipState from './ChipState';
+import ChipTime from './ChipTime';
 import Chip from './Chip';
 import Tab from './Tab';
 import Tag from './Tag';
 import ContainerProgress from './ContainerProgress';
 import Button from './Button';
-import Input from './Input';
+import TabContainer from './TabContainer';
+import CardList from './CardList';
 import BoxSelectGroup from './BoxSelectGroup';
 import BoxSelect from './BoxSelect';
-import Like from './Like';
-import EditButton from './EditButton';
-import Profile from './Profile';
-
+import Input from './Input';
 export {
     BoxSelectGroup,
     BoxSelect,
@@ -23,5 +23,9 @@ export {
     Tag,
     ContainerProgress,
     Button,
+    TabContainer,
+    CardList,
     Input,
+    BoxSelectGroup,
+    BoxSelect,
 };
