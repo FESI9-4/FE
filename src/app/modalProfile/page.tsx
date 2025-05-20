d60196e8ea7eb1de7fd2a79d182c559ed905cb18
jaehyeongjung <titochung625@naver.com>
2025-05-20 16:44:56 +0900

    feat: ModalProfile.tsx 구현

---
+++ src/app/modalProfile/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useState } from 'react';
+import ModalProfile from '@/components/ui/ModalProfile';
+
+export default function ModalTestPage() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpen = () => setIsOpen(true);
+    const handleClose = () => setIsOpen(false);
+    const handleSubmit = () => {
+        console.log('수정 완료!');
+        setIsOpen(false); 
+    };
+
+    return (
+        <div className="text-3xl font-bold">
+            <button onClick={handleOpen}>모달 열기</button>
+            {isOpen && <ModalProfile onClose={handleClose} onSubmit={handleSubmit} />}
+        </div>
+    );
+}
