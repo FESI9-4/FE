2a3ec30b15b06d47475a9f2d34ef60981238c749
Accept77 <accept_agreed@kakao.com>
2025-05-22 15:22:02 +0900

    refactor: tag done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 'use client';
 
+import {
+    Button,
+    Chip,
+    ChipInfo,
+    ChipState,
+    ChipTime,
+    ContainerProgress,
+    Tag,
+} from '@/components/ui';
+
 export default function Home() {
-    return <div className="text-3xl font-bold bg-black h-screen"></div>;
+    return (
+        <div className="text-3xl font-bold bg-black h-screen flex flex-col gap-4 p-10">
+            <Tag>오늘 12시 마감</Tag>
+            <ContainerProgress max={100} current={50} openStatus={true} />
+            <Button>버튼</Button>
+            <Chip>칩</Chip>
+            <ChipTime>칩타임</ChipTime>
+            <ChipInfo>칩정보</ChipInfo>
+            <ChipState>칩상태</ChipState>
+        </div>
+    );
 }
