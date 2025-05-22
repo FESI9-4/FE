ff8ab5bb4050775646ff6cc3c1af0b9917242844
GitHub Oaelan <1djb23@naver.com>
2025-05-22 17:43:34 +0900

    feat: 비제어/제어 선택가능하게 사용 가능하있도록 구현

--- src/components/ui/TextField.tsx
+++ src/components/ui/TextField.tsx
@@ -1,120 +1,139 @@
 'use client';
-import { InputHTMLAttributes, HTMLAttributes, useRef, useState } from 'react';
-import { type VariantProps } from 'class-variance-authority';
-import Label from './Label';
-import { LabelVariants } from './Label';
-import Input, { InputVariants } from '../Input';
-import HelperText, { HelperTextVariants } from './HelperText';
+import { InputHTMLAttributes, forwardRef, useState } from 'react';
+import Label, { LabelProps } from './Label';
+import Input, { InputProps } from './Input';
+import HelperText, { HelperTextProps } from './HelperText';
 // 컴포넌트의 Props 타입 정의
 interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
     value?: string; // 인풋 값
     label?: string; // 레이블 텍스트
-    isError?: boolean; // 오류 상태
     helperText?: string; // 오류 메시지
     slotProps?: {
-        //라벨,인풋,헬퍼 텍스트 관련 프롭스
-        input?: InputHTMLAttributes<HTMLInputElement> &
-            VariantProps<typeof InputVariants>; // HTML input 속성
-        inputLabel?: HTMLAttributes<HTMLLabelElement> &
-            VariantProps<typeof LabelVariants>; // HTMLLabelElement 속성으로 변경
-        helperText?: HTMLAttributes<HTMLSpanElement> &
-            VariantProps<typeof HelperTextVariants>; // HTMLSpanElement 속성으로 변경
+        // 라벨, 인풋, 헬퍼 텍스트 관련 프롭스
+        input?: InputProps;
+        inputLabel?: LabelProps;
+        helperText?: HelperTextProps;
     };
 }
 
-export default function TextField(props: TextFieldProps) {
-    const {
-        value, //제어 컴포넌트 일 경우 사용,
-        onChange, //제어 컴포넌트 일 경우 사용
-        isError = false, // 오류 상태
-        label, // 라벨안 내용
-        helperText, // 헬퍼 텍스트
-        slotProps, // 라벨,인풋,헬퍼 텍스트 관련 프롭스
-        ...rest
-    } = props;
-    //라벨 관련 프룹스
-    const labelProps = {
-        htmlFor: slotProps?.inputLabel?.id,
-        labelSize: slotProps?.inputLabel?.labelSize,
-        children: label,
-        className: slotProps?.inputLabel?.className,
-    };
+// forwardRef를 사용하여 외부에서 ref를 받을 수 있도록 수정
+export default forwardRef<HTMLInputElement, TextFieldProps>(
+    function TextField(props, ref) {
+        const {
+            value,
+            defaultValue,
+            label,
+            helperText,
+            slotProps,
+            onFocus: onFocusProp,
+            onBlur: onBlurProp,
+            ...rest
+        } = props;
 
-    // 비제어 컴포넌트를 위한 ref
-    const inputRef = useRef<HTMLInputElement>(null);
-    // 비제어 컴포넌트에서 사용할 isError 상태
-    const [uncontrolError, setUncontrolError] = useState<boolean | null>(null);
-    // 비제어 컴포넌트에서 사용되는 changeHandler
-    function uncontrolledHandleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const { value, type } = e.target;
-        // 제어일경우 상위에서 제공한 onChange 사용
-        if (onChange) {
-            onChange(e);
-            return;
-        }
-        if (inputRef.current !== null) {
-            console.log('uncontroll');
-            inputRef.current.value = value;
-            switch (type) {
-                case 'file':
-                    const file = e.target.files?.[0]; // 선택된 파일 가져오기
-                    if (file) {
-                        const validImageTypes = [
-                            'image/jpeg',
-                            'image/png',
-                            'image/gif',
-                        ]; // 허용할 이미지 타입
-                        if (!validImageTypes.includes(file.type)) {
-                            setUncontrolError(true); // 이미지가 아닐 경우 오류 상태 설정
-                            inputRef.current.value = ''; // 입력 필드 초기화
-                        } else {
-                            setUncontrolError(false); // 유효한 이미지 파일일 경우 오류 상태 해제
-                        }
-                    }
-                    break;
-                case 'number':
-                    // 숫자 타입인 경우 최대 최소값
-                    if (value && Number(value) < 0) {
-                        setUncontrolError(true);
-                        inputRef.current.value = '0';
-                    } else if (value && Number(value) >= 999) {
-                        setUncontrolError(true);
-                        inputRef.current.value = '999';
-                    } else {
-                        //0 ~ 999 사이의 숫자일 경우 오류 상태 해제
-                        setUncontrolError(false);
-                    }
-                    break;
+        // value가 있으면 제어 컴포넌트, 없으면 비제어 컴포넌트
+        const isControlled = value !== undefined;
+        //외부에서 전달받은 Variant
+        const outerVariant = slotProps?.input?.variant;
+        //나중에 훅으로 따로 분리 할 예정
+        const [ShowHelperText, setShowHelperText] = useState(false);
+        const [InnerVariant, setInnerVariant] = useState<
+            'default' | 'done' | 'typing' | 'error'
+        >('default');
+
+        const isVariantControlled = outerVariant !== undefined;
+        const currentVariant = isVariantControlled
+            ? outerVariant
+            : InnerVariant;
+
+        // 포커스 이벤트 핸들러
+        function handleFocus(e: React.FocusEvent<HTMLInputElement>) {
+            //제어 * 상위 컴포넌트 이벤트 먼저 실행
+            onFocusProp?.(e);
+            //비제어
+            if (!isControlled) {
+                setInnerVariant('typing');
             }
         }
+        // 블러 이벤트 핸들러
+        function handleBlur(e: React.FocusEvent<HTMLInputElement>) {
+            const { name, value } = e.target;
+            // 상위 컴포넌트의 onBlur 호출 (있다면)
+            if (onBlurProp) {
+                onBlurProp(e);
+                return;
+            }
+            console.log('UnControlled handleBlur');
+
+            // name에 따라 입력 유효성 검사 (type으로 검사하는 것보다 더 유연할것 같슴다)
+            switch (name) {
+                case 'email':
+                    if (value.includes('@')) {
+                        setInnerVariant('done');
+                        setShowHelperText(false);
+                    } else {
+                        setInnerVariant('error');
+                        setShowHelperText(true);
+                    }
+                    break;
+                case 'name':
+                    if (value.length > 10) {
+                        console.log('done');
+                        setInnerVariant('done');
+                        setShowHelperText(false);
+                    } else {
+                        console.log('error');
+                        setInnerVariant('error');
+                        setShowHelperText(true);
+                    }
+                    break;
+                default:
+                    setInnerVariant('done');
+                // setShowHelperText(false);
+            }
+            //setInputVariant('done');
+        }
+
+        // value와 defaultValue가 모두 있을 경우 경고 (둘 중 하나만 선택해야함)
+        if (process.env.NODE_ENV !== 'production') {
+            if (isControlled && defaultValue !== undefined) {
+                console.warn(
+                    'TextField는 제어 컴포넌트로 사용될 때(value 속성 제공) defaultValue를 무시합니다.'
+                );
+            }
+        }
+        //인풋 관련 프룹스
+        const inputProps = {
+            variant: currentVariant,
+            isControlled: isControlled,
+            // 제어/비제어 모드에 따라 value 또는 defaultValue 전달
+            ...(isControlled
+                ? { value }
+                : { defaultValue: defaultValue || '' }),
+            ref: ref, // 외부에서 전달받은 ref를 Input에 전달
+            onFocus: handleFocus,
+            onBlur: handleBlur,
+            ...rest,
+        };
+        //라벨 관련 프룹스
+        const labelProps = {
+            children: label,
+            labelSize: slotProps?.inputLabel?.labelSize,
+            ...slotProps?.inputLabel,
+        };
+        //헬퍼 텍스트 관련 프룹스
+        const helperTextProps = {
+            children: helperText,
+            className: slotProps?.helperText?.className,
+            isShow: slotProps?.helperText?.isShow || ShowHelperText,
+            ...slotProps?.helperText,
+        };
+
+        return (
+            <div className="flex flex-col w-full">
+                <Label {...labelProps} />
+                <Input {...inputProps} />
+                <HelperText {...helperTextProps} />
+            </div>
+        );
     }
-    //인풋 관련 프룹스
-    const inputProps = {
-        id: slotProps?.input?.id,
-        type: slotProps?.input?.type,
-        placeholder: slotProps?.input?.placeholder,
-        className: slotProps?.input?.className,
-        // 제어일 경우 위에서 제공한 isError 사용
-        // 비제어일 경우 uncontrolError 사용
-        isError: value === null ? uncontrolError : isError,
-        // value가 있을 경우에만 설정 (상위에서 값을 줄 경우 제어 안주면 비제어)
-        defaultValue: value === undefined ? '' : value,
-        // 상위에서 onChange 핸들러를 제공한다면 해당 헨들러 사용
-        // 제공하지 않는다면 inputRef를 통해 비제어 컴포넌트로 처리
-        onChange: uncontrolledHandleChange,
-        ref: inputRef, // ref를 인풋에 전달
-        ...rest,
-    };
-    //헬퍼 텍스트 관련 프룹스
-    const helperTextProps = {
-        className: slotProps?.helperText?.className,
-        ...rest,
-    };
-    return (
-        <div className="flex flex-col w-full ">
-            <Label {...labelProps}>{label}</Label>
-            <Input {...inputProps} />
-            <HelperText {...helperTextProps}>{helperText}</HelperText>
-        </div>
-    );
-}
+);
