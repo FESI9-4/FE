a9355a3e2e8856e554e786d2ce9a921eb844da52
Accept77 <accept_agreed@kakao.com>
2025-05-22 16:31:38 +0900

    refactor conainerProgress done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,25 @@ export default function Home() {
     return (
         <div className="text-3xl font-bold bg-black h-screen flex flex-col gap-4 p-10">
             <Tag>오늘 12시 마감</Tag>
-            <ContainerProgress max={100} current={50} openStatus={true} />
+            <ContainerProgress
+                max={10}
+                current={3}
+                openStatus="waiting"
+                deadline="1월 5일 12:00"
+            />
+            <ContainerProgress
+                max={10}
+                current={5}
+                openStatus="progressing"
+                deadline="1월 5일 12:00"
+            />
+            <ContainerProgress
+                max={10}
+                current={10}
+                openStatus="finished"
+                deadline="1월 5일 12:00"
+            />
+
             <Button>버튼</Button>
             <Chip>칩</Chip>
             <ChipTime>칩타임</ChipTime>
--- src/components/ui/ContainerProgress.tsx
+++ src/components/ui/ContainerProgress.tsx
@@ -1,30 +1,79 @@
-import clsx from 'clsx';
+import { cva } from 'class-variance-authority';
 import { motion } from 'framer-motion';
+import { CheckIcon, UserIcon } from '@/assets';
 
 interface ContainerProgressProps {
     max: number;
     current: number;
-    openStatus: boolean;
+    openStatus: 'waiting' | 'finished' | 'progressing';
+    deadline: string;
 }
 
 export default function ContainerProgress({
     max,
     current,
     openStatus,
+    deadline,
 }: ContainerProgressProps) {
     const progress = `${((current / max) * 100).toFixed(0)}%`;
-    const className = clsx(
-        'h-1 rounded-md border-none',
-        openStatus ? 'bg-orange-600' : 'bg-orange-400'
-    );
+    const className = cva('h-2 rounded-md border-none', {
+        variants: {
+            openStatus: {
+                waiting: 'bg-gray-300',
+                progressing: 'bg-green-400',
+                finished: 'bg-green-800',
+            },
+        },
+        defaultVariants: {
+            openStatus: 'waiting',
+        },
+    });
+
     return (
-        <div className="w-full h-1 bg-orange-50 rounded-md overflow-hidden">
-            <motion.hr
-                className={className}
-                initial={{ width: '0%' }}
-                animate={{ width: progress }}
-                transition={{ duration: 1, ease: 'easeOut' }}
-            />
+        <div className="flex flex-col gap-2">
+            <div className="flex justify-between text-gray-50 text-sm font-medium">
+                <div className="flex gap-4 items-center justify-center">
+                    <div className="flex gap-0.5 items-center justify-center">
+                        <UserIcon width={16} height={16} />
+                        <div>
+                            {current} / {max}
+                        </div>
+                    </div>
+                    {openStatus === 'progressing' && (
+                        <div className="flex gap-0.5 items-center justify-center text-green-400">
+                            <div>개설확정</div>
+                            <CheckIcon
+                                width={16}
+                                height={16}
+                                className="fill-gray-900"
+                            />
+                        </div>
+                    )}
+                </div>
+                {openStatus === 'finished' ? (
+                    <div className="flex gap-0.5 items-center justify-center text-green-400">
+                        <div>모집마감</div>
+                        <CheckIcon
+                            width={16}
+                            height={16}
+                            className="text-black fill-green-500"
+                        />
+                    </div>
+                ) : (
+                    <div className="flex gap-2 items-center justify-center">
+                        <div className="text-green-400">마감</div>
+                        <div>{deadline}</div>
+                    </div>
+                )}
+            </div>
+            <div className="w-full h-2 bg-gray-800 rounded-md overflow-hidden">
+                <motion.hr
+                    className={className({ openStatus })}
+                    initial={{ width: '0%' }}
+                    animate={{ width: progress }}
+                    transition={{ duration: 1, ease: 'easeOut' }}
+                />
+            </div>
         </div>
     );
 }
