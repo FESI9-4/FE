45277da606b04c861439bc60fb54b9d34c132651
jaehyeongjung <titochung625@naver.com>
2025-05-23 14:34:59 +0900

    chore: react-calender 추가

--- src/components/ui/ModalProfile.tsx
+++ src/components/ui/ModalProfile.tsx
@@ -13,42 +13,31 @@ interface ModalProfileProps {
 export default function ModalProfile({ onClose, onSubmit }: ModalProfileProps) {
     const modalRef = useRef<HTMLDivElement>(null);
     return (
-        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-[#00000099] backdrop-blur-sm flex items-center justify-center z-50">
             <div
                 ref={modalRef}
-                className="w-85.75 h-81 md:w-130 md:h-82 flex items-center justify-center rounded-xl bg-white"
+                className="w-85.75 h-82 md:w-130 md:h-84 flex items-center justify-center rounded-xl bg-gray-800 text-white"
                 onClick={(e) => e.stopPropagation()}
             >
-                <div className="w-73.75 h-69 md:w-118 md:h-70 flex flex-col gap-8 md:gap-6">
-                    <div className="flex justify-between h-7 w-full items-center">
+                <div className="w-73.75 h-70 md:w-118 md:h-72 flex flex-col gap-6">
+                    <div className="flex justify-between h-7 w-full items-center ">
                         <p className="text-lg font-semibold">프로필 수정하기</p>
-                        <button
-                            className="w-6 h-6 cursor-pointer"
-                            onClick={onClose}
-                        >
-                            <Image
-                                src="/icons/deleteIcon.svg"
-                                alt="delete"
-                                width={24}
-                                height={24}
-                            ></Image>
-                        </button>
                     </div>
-                    <div className="flex flex-col gap-3 md:gap-6 h-39 md:h-40">
+                    <div className="flex flex-col gap-6 h-41 ">
                         <div>
                             <Image
                                 src="/icons/profileEdit.svg"
                                 alt="profileEdit"
-                                width={56}
-                                height={56}
+                                width={64}
+                                height={64}
                             ></Image>
                         </div>
-                        <div className="h-19 md:h-20">
-                            <p className="text-base font-semibold">회사</p>
+                        <div className="h-19">
+                            <p className="text-base font-semibold">닉네임</p>
                             <Input type="text" id="text"></Input>
                         </div>
                     </div>
-                    <div className="flex gap-4 h-11">
+                    <div className="flex gap-3 h-10 md:h-12">
                         <Button size="large" onClick={onClose}>
                             취소
                         </Button>
