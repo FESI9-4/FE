64e2945161effe04757d268d9b68900b12b77883
Accept77 <accept_agreed@kakao.com>
2025-05-23 16:46:27 +0900

    refactor: components refactor done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-'use client';
-
-import { EditButton } from '@/components/ui';
-
 export default function Home() {
-    return (
-        <div className="text-3xl font-bold">
-            <EditButton onClick={() => {}} size="large" />
-            <EditButton onClick={() => {}} size="small" />
-        </div>
-    );
+    return <div className="text-3xl font-bold">main</div>;
 }
