65876f16360b13945c1b86c5517bf8e200e4e82e
GitHub Oaelan <1djb23@naver.com>
2025-05-25 02:39:59 +0900

    feat: 간단한 유효성 검사 유틸함수 추가

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,22 +1,15 @@
+import { InputValue } from '@/store/textfieldStore';
+
 export const validators = {
     //이메일 유효성 검사
-    validateEmail: function (email: string): {
+    validateEmail: function (email: InputValue): {
         isValid: boolean;
         message: string;
     } {
-        /*   ^ / $ : 입력 시작과 종료
-        [0-9a-zA-Z] : 숫자, 영어소문자, 영어대문자 포함 
-        ([-_.]?[0-9a-zA-Z])*- [-_.]? : '-_.' 특수문자는 없거나 하나만 포함 가능
-        *@ :무조건 골뱅이 포함
-        .[a-zA-Z]{2,3} : . 다음에 영어소문자와 영어대문자가 2개 or 3개 가능
-        /i : 대소문자 구분 X
-         */
-
         //이메일 정규식 통과후  -> DB에 중복 이메일 검사 후 통과
         const emailRegex =
             /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-
-        switch (emailRegex.test(email)) {
+        switch (emailRegex.test(email as string)) {
             case true:
                 return { isValid: true, message: '성공' };
                 break;
@@ -30,16 +23,16 @@ export const validators = {
         }
     },
     //닉네임 유효성 검사
-    validateNickname: function (nickname: string): {
+    validateNickname: function (nickname: InputValue): {
         isValid: boolean;
         message: string;
     } {
         //한글 영어 대소문자 숫자 공백 포함 2~8자
         const nicknameRegex = /^[가-힣a-zA-Z0-9\s]{2,8}$/;
         /*
-        비속어필터 같은걸 활용하면 좋을듯 정식 통과 후 비속어 필터 통과 후 통과 후 중복 닉네임 검사 후 통과
+        정규식 통과 후 -> 중복 닉네임 검사 후 통과
         */
-        switch (nicknameRegex.test(nickname)) {
+        switch (nicknameRegex.test(nickname as string)) {
             case true:
                 return { isValid: true, message: '성공' };
                 break;
@@ -53,33 +46,34 @@ export const validators = {
         }
     },
     //비밀번호 유효성 검사
-    validatePassword: function (password: string): {
+    validatePassword: function (password: InputValue): {
         isValid: boolean;
         message: string;
     } {
         //최소 8자 ~ 16자, 최소 하나의 대문자 포함 및 소문자 포함 및 특수문자 포함
         const passwordRegex =
             /^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/;
-        switch (passwordRegex.test(password)) {
+        switch (passwordRegex.test(password as string)) {
             case true:
                 return { isValid: true, message: '성공' };
                 break;
             case false:
                 return {
                     isValid: false,
-                    message: '비밀번호는 8~16자 이내로 입력해주세요.',
+                    message:
+                        '대소문자, 특수문자를 포함하여 8~16자 이내로 입력해주세요.',
                 };
                 break;
         }
     },
     //이미지 유효성 검사
-    validateImage: function (image: string): {
+    validateImage: function (fileType: InputValue): {
         isValid: boolean;
         message: string;
     } {
-        //이미지 파일 확장자 검사
-        const imageRegex = /\.(jpg|jpeg|png|gif|bmp|webp)$/i;
-        switch (imageRegex.test(image)) {
+        //이미지 파일 확장자 및(용량) 검사
+        const imgRegex = /^image\/(jpg|jpeg|png|gif|bmp|webp)$/i;
+        switch (imgRegex.test(fileType as string)) {
             case true:
                 return { isValid: true, message: '성공' };
                 break;
@@ -92,13 +86,13 @@ export const validators = {
         }
     },
     //넘버 타입 유효성 검사
-    validateNumber: function (number: string): {
+    validateNumber: function (number: InputValue): {
         isValid: boolean;
         message: string;
     } {
         //숫자만 입력 가능 (지수표현 이런거 허용 X)
         const numberRegex = /^[0-9]+$/;
-        switch (numberRegex.test(number)) {
+        switch (numberRegex.test(number as string)) {
             case true:
                 return { isValid: true, message: '성공' };
                 break;
