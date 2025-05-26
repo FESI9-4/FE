ba8988f3bb45feb9ff6e7a2c7216d747e8175acd
Accept77 <accept_agreed@kakao.com>
2025-05-26 17:21:49 +0900

    feat: cardList done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,44 @@ import { CardList } from '@/components/ui';
 
 export default function Home() {
     return (
-        <div className="text-3xl font-bold">
+        <div className="text-3xl font-bold h-full bg-black">
             <CardList
-                title="테스트 타이틀"
+                title="데이브레이크 콘서트"
                 location="홍대입구역 8번출구"
                 date="1747361368"
                 deadline="1748361368"
-                createdAt="1746361368"
-                currentPerson={1}
+                currentPerson={3}
                 maxPerson={10}
-                openStatus="open"
+                openStatus="waiting"
                 wishList={false}
-                image="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbWpwxV%2FbtsNKx9Q0JL%2Fckx0ZMKTLsfbcW2WQTCw4k%2Fimg.png"
-                useStatus="use"
-                createdUser="홍길동"
+                image="https://www.kopis.or.kr/upload/pfmPoster/PF_PF265653_250523_132834.gif"
+                createdUser="뫀호호"
+                createdUserProfileImg="https://blog.kakaocdn.net/dn/bedWRa/btsNEEWt0FW/qFXWdARM0Q6oq2fHvbu79k/img.gif"
+            />
+            <CardList
+                title="데이브레이크 콘서트"
+                location="홍대입구역 8번출구"
+                date="1747361368"
+                deadline="1748361368"
+                currentPerson={7}
+                maxPerson={10}
+                openStatus="progressing"
+                wishList
+                image="https://otr.co.kr/wp-content/uploads/mangboard/2021/10/31/F72572_%EC%9B%B9%ED%8F%AC%EC%8A%A4%ED%84%B0_%EC%A0%95%EC%82%AC%EA%B0%81%ED%98%95_jpg.jpg"
+                createdUser="뫀호호"
+                createdUserProfileImg="https://blog.kakaocdn.net/dn/bedWRa/btsNEEWt0FW/qFXWdARM0Q6oq2fHvbu79k/img.gif"
+            />
+            <CardList
+                title="데이브레이크 콘서트"
+                location="홍대입구역 8번출구"
+                date="1747361368"
+                deadline="1748361368"
+                currentPerson={7}
+                maxPerson={10}
+                openStatus="finished"
+                wishList={false}
+                image="https://blog.kakaocdn.net/dn/bedWRa/btsNEEWt0FW/qFXWdARM0Q6oq2fHvbu79k/img.gif"
+                createdUser="뫀호호"
                 createdUserProfileImg="https://blog.kakaocdn.net/dn/bedWRa/btsNEEWt0FW/qFXWdARM0Q6oq2fHvbu79k/img.gif"
             />
         </div>
