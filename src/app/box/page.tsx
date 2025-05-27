9c8bf883a8571dcf1fc4002dcafccbf4f4ca918d
GitHub Oaelan <1djb23@naver.com>
2025-05-27 18:49:41 +0900

    fix:빌드에러 고치는중..

--- src/app/box/page.tsx
+++ src/app/box/page.tsx
@@ -1,8 +1,21 @@
-import { BoxSelectGroup } from '@/components/ui';
-import { CATEGORY_DATA } from '@/types/categories';
+import { GoTogetherIcon } from '@/assets';
 
-function BoxPage() {
-    return <BoxSelectGroup name="test" categories={CATEGORY_DATA} />;
+export default function BoxPage() {
+    return (
+        <div className="w-screen h-screen">
+            <div className="w-full h-full bg-gray-100">
+                <div>
+                    <label>선택 서비스</label>
+                    <div>
+                        <span>같이가요</span>
+                        <GoTogetherIcon
+                            width={20}
+                            height={20}
+                            className="text-gray-500"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
-
-export default BoxPage;
