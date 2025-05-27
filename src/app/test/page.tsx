cb340cb3da5a41378d04c66c435c89ba61606e0b
jaehyeongjung <titochung625@naver.com>
2025-05-27 15:36:15 +0900

    refactor: button 컴포넌트 className 뚫기

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,7 +4,7 @@ import ProfileSection from '@/components/ui/ProfileSection';
 
 export default function TestPage() {
     return (
-        <div className='bg-white mt-30 ml-40 '>
+        <div className="bg-white mt-30 ml-40 ">
             <ProfileSection></ProfileSection>
         </div>
     );
