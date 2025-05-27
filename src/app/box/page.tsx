f04c1af81ae1c10007a35bae3daf5ffbb872d490
GitHub Oaelan <1djb23@naver.com>
2025-05-27 20:01:57 +0900

    chore: 커밋정리중

--- src/app/box/page.tsx
+++ src/app/box/page.tsx
@@ -1,17 +1,12 @@
 import { GoTogetherIcon } from '@/assets';
 
-export default function BoxPage() {
+function BoxPage() {
     return (
-        <div className="w-screen h-screen">
-            <div className="w-full h-full bg-gray-100">
-                <div>
-                    <label>선택 서비스</label>
-                    <div>
-                        <span>같이가요</span>
-                        <GoTogetherIcon className="text-gray-500" />
-                    </div>
-                </div>
-            </div>
+        <div>
+            BoxPage
+            <GoTogetherIcon />
         </div>
     );
 }
+
+export default BoxPage;
