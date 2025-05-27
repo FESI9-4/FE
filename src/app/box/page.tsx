c084714eae197b8e398c7e2e52e4e3c1cf9283ca
GitHub Oaelan <1djb23@naver.com>
2025-05-27 19:02:09 +0900

    fix:빌드 오류 해결중

--- src/app/box/page.tsx
+++ src/app/box/page.tsx
@@ -8,11 +8,7 @@ export default function BoxPage() {
                     <label>선택 서비스</label>
                     <div>
                         <span>같이가요</span>
-                        <GoTogetherIcon
-                            width={20}
-                            height={20}
-                            className="text-gray-500"
-                        />
+                        <GoTogetherIcon className="text-gray-500" />
                     </div>
                 </div>
             </div>
