73f1b2584f15f9ca49d2c52694aba85db1dd500e
GitHub Oaelan <1djb23@naver.com>
2025-05-28 10:53:17 +0900

    refactor: BoxSelct 리팩토링

--- src/app/box/page.tsx
+++ src/app/box/page.tsx
@@ -1,10 +1,21 @@
-import { GoTogetherIcon } from '@/assets';
+'use client';
+import { FormEvent } from 'react';
+import BoxSelect from '@/components/ui/BoxSelect';
+import { CATEGORY_DATA } from '@/types/categories';
 
 function BoxPage() {
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const serviceValue = formData.get('selectedCategory'); // "GO_TYPE_BUSRENTAL_TYPE"
+        console.log(serviceValue);
+    };
     return (
-        <div>
-            BoxPage
-            <GoTogetherIcon />
+        <div className="flex flex-col h-screen items-center justify-center">
+            <form onSubmit={handleSubmit}>
+                <BoxSelect categories={CATEGORY_DATA} />
+                <button type="submit">제출</button>
+            </form>
         </div>
     );
 }
