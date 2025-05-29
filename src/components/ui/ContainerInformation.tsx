fbdf076afbdcb2608b0a49b4c712baed3244de52
jaehyeongjung <titochung625@naver.com>
2025-05-29 17:09:55 +0900

    feat: props 뚫기, 날짜변환, stauts, like 기능

--- src/components/ui/ContainerInformation.tsx
+++ src/components/ui/ContainerInformation.tsx
@@ -1,31 +1,78 @@
 import { ProfileIcon, LikeIcon } from '@/assets/index';
 import ContainerProgress from './ContainerProgress';
+import { useState } from 'react';
 
-// 모바일은 하트가 존재하지않음
-// 테블릿부턴 또 있음
-export default function containerInformaiton({}) {
+interface ContainerInformationProps {
+    owner: string;
+    title: string;
+    location: string;
+    actionDate: string;
+    limitedDate: string | number;
+    minPeople: number;
+    maxPeople: number;
+    currentPeople: number;
+}
+export default function ContainerInformaiton({
+    owner,
+    title,
+    location,
+    actionDate,
+    limitedDate,
+    minPeople,
+    maxPeople,
+    currentPeople,
+}: ContainerInformationProps) {
+    const now = new Date();
+    const deadlineDate = new Date(Number(limitedDate) * 1000);
+    const actionDateObj = new Date(Number(actionDate) * 1000);
+    const limitedDateObj = new Date(Number(limitedDate) * 1000);
+    const [liked, setLiked] = useState(false);
+
+    const toggleLike = () => {
+        setLiked((prev) => !prev);
+    };
+
+    const openStatus =
+        now > deadlineDate
+            ? 'finished'
+            : currentPeople >= minPeople
+              ? 'progressing'
+              : 'waiting';
+
+    const formatKoreanDate = (date: Date) =>
+        date.toLocaleString('ko-KR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false,
+        });
     return (
-        <div className="w-full px-2 md:px-0 h-71 md:h-85  flex flex-col gap-7 md:justify-between bg-gray-900 min-w-86">
+        <div className="w-full px-2 md:px-0 h-71 md:h-85  flex flex-col gap-7 md:justify-between bg-transparent min-w-86">
             <div className="h-43  flex flex-col justify-between md:h-48">
                 <div className="h-23 md:h-24  flex flex-col gap-2 md:gap-3">
                     <div className=" flex flex-col h-15 gap-1 ">
                         <div className="flex justify-between items-center h-8">
                             <span className="text-2xl font-semibold h-8">
-                                데이식스 콘서트 버스 대절
+                                {title}
                             </span>
                             <LikeIcon
                                 width={24}
                                 height={24}
-                                className="hidden md:inline-block  m-2"
-                            ></LikeIcon>
+                                onClick={toggleLike}
+                                className={`hidden md:inline-block m-2 hover:scale-105 cursor-pointer transition-colors duration-100 ${
+                                    liked ? 'fill-white' : 'fill-transparent'
+                                }`}
+                            />
                         </div>
                         <span className="text-base font-normal text-gray-300 h-6 ">
-                            을지로 3가 서울시 중구 청계천로 100
+                            {location}
                         </span>
                     </div>
                     <div className="h-6 flex gap-2 items-center ">
                         <ProfileIcon width={32} height={32} />
-                        <span className="h-5">데식이들</span>
+                        <span className="h-5">{owner}</span>
                     </div>
                 </div>
                 <div className="h-13 md:h-14  flex flex-col gap-1 md:gap-2">
@@ -36,7 +83,7 @@ export default function containerInformaiton({}) {
                         <div className="w-0.5 h-3.5 ml-2.5 mr-2 bg-gray-800" />
 
                         <span className="text-base font-normal">
-                            2025년 1월 7일 12:00
+                            {formatKoreanDate(actionDateObj)}
                         </span>
                     </div>
                     <div className="h-6 flex items-center">
@@ -46,7 +93,7 @@ export default function containerInformaiton({}) {
                         <div className="w-0.5 h-3.5 ml-2.5 mr-2 bg-gray-800" />
 
                         <span className="text-base font-normal">
-                            2025년 1월 7일 12:00
+                            {formatKoreanDate(limitedDateObj)}
                         </span>
                     </div>
                 </div>
@@ -54,15 +101,21 @@ export default function containerInformaiton({}) {
             <div className="h-21 md:h-20 flex flex-col gap-3">
                 <div className="h-10 ">
                     <ContainerProgress
-                        max={20}
-                        current={18}
-                        openStatus="progressing"
-                        deadline="2025.06.30"
+                        max={maxPeople}
+                        current={currentPeople}
+                        openStatus={openStatus}
+                        deadline={deadlineDate.toLocaleString('ko-KR', {
+                            month: 'long',
+                            day: 'numeric',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            hour12: false,
+                        })}
                     ></ContainerProgress>
                 </div>
                 <div className="h-4 flex items-center justify-between text-sm font-medium text-gray-500">
-                    <span className="">최소인원 5명</span>
-                    <span>최대인원 10명</span>
+                    <span>최소인원 {minPeople}명</span>
+                    <span>최대인원 {maxPeople}명</span>
                 </div>
             </div>
         </div>
