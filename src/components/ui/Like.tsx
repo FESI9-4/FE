e956fa3af9d4997817949f9ac8b07557f613a927
jaehyeongjung <titochung625@naver.com>
2025-05-29 18:06:04 +0900

    refactor: like 컴포넌트 cn 뚫기 -> 모바일 hidden 값

--- src/components/ui/Like.tsx
+++ src/components/ui/Like.tsx
@@ -1,17 +1,20 @@
 import { LikeIcon } from '@/assets';
 import { ButtonHTMLAttributes } from 'react';
 import { cva } from 'class-variance-authority';
+import { cn } from '@/utils/cn'; 
 
 interface LikeProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     like?: boolean;
+    className?: string;
 }
 
-export default function Like({ like, onClick }: LikeProps) {
-    const className = cva('hover:cursor-pointer', {
+export default function Like({ like, onClick, className }: LikeProps) {
+    const baseClass = cva('hover:cursor-pointer', {
         variants: {
             like: {
                 true: 'text-green-400',
                 false: 'text-gray-400',
+                
             },
         },
         defaultVariants: {
@@ -20,7 +23,7 @@ export default function Like({ like, onClick }: LikeProps) {
     });
 
     return (
-        <button onClick={onClick} className={className({ like })}>
+        <button onClick={onClick}   className={cn(baseClass({ like }), className)}>
             <LikeIcon
                 width={24}
                 height={24}
