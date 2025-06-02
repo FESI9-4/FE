87275569138cfdbcf9202a3dc82914c46e46fb80
jaehyeongjung <titochung625@naver.com>
2025-06-02 18:15:54 +0900

    feat: TapSection의 반응형 작업

--- src/components/main/TapSeciton.tsx
+++ src/components/main/TapSeciton.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 export default function TapSeciton() {
     const [activeTab, setActiveTab] = useState<number>(0);
     return (
-        <div className='w-full h-23 bg-blue-700 sm:h-31 xl:h-53 flex justify-center'>
+        <div className="w-full h-23 bg-blue-700 sm:h-31 xl:h-53 flex justify-center">
             <div className="w-full h-12 flex xl:w-70 bg-amber-600">
                 <Tab
                     icon={<GoTogetherIcon width={24} height={24} />}
