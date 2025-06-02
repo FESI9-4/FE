96f8b246d5e01796c74a2ccc96d1357205ebdf10
GitHub Oaelan <1djb23@naver.com>
2025-06-02 15:01:56 +0900

    remvoe: 필요없는 폴더 삭제

--- src/app/box/page.tsx
+++
@@ -1,8 +0,0 @@
-import { BoxSelectGroup } from '@/components/ui';
-import { CATEGORY_DATA } from '@/types/categories';
-
-function BoxPage() {
-    return <BoxSelectGroup name="test" categories={CATEGORY_DATA} />;
-}
-
-export default BoxPage;
