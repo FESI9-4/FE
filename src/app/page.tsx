97e512fbd5e66ca1da8f4761ab79d154aeedf146
jaehyeongjung <titochung625@naver.com>
2025-06-02 15:14:22 +0900

    refactor: Nav fixed 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <div className="text-3xl font-bold">main</div>;
+    return (
+        <div className="w-full h-362.5 px-4 sm:px-8 xl:px-10 bg-[#14151A] pt-6 sm:pt-5 xl:pt-8">
+            <div className="bg-blue-300 w-full h-362.5"></div>
+        </div>
+    );
 }
