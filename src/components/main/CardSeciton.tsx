533508454b8a23728b9ba845a482111c8f2c634a
jaehyeongjung <titochung625@naver.com>
2025-06-02 18:15:54 +0900

    feat: TapSection의 반응형 작업

--- src/components/main/CardSeciton.tsx
+++ src/components/main/CardSeciton.tsx
@@ -1,5 +1,9 @@
 'use client';
 
 export default function CardSeciton() {
-    return  <div className="w-full h-335.5 sm:h-341 xl:h-331 bg-red-300">카드영역</div>;
+    return (
+        <div className="w-full h-335.5 sm:h-341 xl:h-331 bg-red-300">
+            카드영역
+        </div>
+    );
 }
