81e9a5f48ce95371c8565a7a4402c7ac05347529
GitHub Oaelan <1djb23@naver.com>
2025-06-04 01:24:03 +0900

    feat: 비밀번호 찾기 페이지 구현

---
+++ src/app/(auth)/findpassword/page.tsx
@@ -0,0 +1,11 @@
+import LoginIllustration from '../LoginIllustration';
+import FindPasswordForm from './FindPasswordForm';
+
+export default function FindPasswordPage() {
+    return (
+        <div className="flex flex-col xl:flex-row xl:gap-[186px] gap-20 justify-center bg-[#14151A] min-h-screen min-w-screen px-4">
+            <LoginIllustration />
+            <FindPasswordForm />
+        </div>
+    );
+}
