51f23b72bc82dc2d6f7e376e2f2a24db83854072
GitHub Oaelan <1djb23@naver.com>
2025-06-04 01:22:44 +0900

    feat: LoginIllustration className props 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,11 @@
 'use client';
-import LeftImg from '../LoginIllustration';
+import LoginIllustration from '../LoginIllustration';
 import LoginForm from './LoginForm';
 
 function LoginPage() {
     return (
         <div className="flex flex-col xl:flex-row xl:gap-[186px] justify-center bg-[#14151A] min-h-screen min-w-screen px-4">
-            <LeftImg />
+            <LoginIllustration />
             <LoginForm />
         </div>
     );
