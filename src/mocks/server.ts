f188c54a88dc252569909f77f2fda06d4c2cb3cc
GitHub Oaelan <1djb23@naver.com>
2025-06-04 20:35:45 +0900

    refactor: 오류 수정

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,5 +1,5 @@
 import { setupServer } from 'msw/node';
-import { usersHandlers } from './handlers/posts';
+import { postsHandlers } from './handlers/posts';
 
 // MSW 서버를 설정합니다.
-export const server = setupServer(...usersHandlers);
+export const server = setupServer(...postsHandlers);
