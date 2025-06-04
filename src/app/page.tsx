7de348b4d1aaba799dde3c767dbaa3f428f31867
jaehyeongjung <titochung625@naver.com>
2025-06-04 17:26:51 +0900

    feat: 최종 레이아웃 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import TabSection from '@/components/main/TapSeciton';
 import FilterSeciton from '@/components/main/FilterSeciton';
 import CardSection from '@/components/main/CardSeciton';
+import { mockCards } from '@/__mock__/cards';
+
+//TODO api 연결부 필터링, 카드 데이터, 페이지네이션,
+// 우선 레이아웃 pr 먼저 -> api 작업완료 순으로 진행예정
 
 export default function Home() {
     return (
-        <div className="w-full h-411.5 sm:h-433.5 xl:h-468.25 px-4 sm:px-6  bg-yellow-300 pt-6 sm:pt-5 xl:pt-8 flex justify-center min-w-85.5">
-            <div className="bg-blue-300 w-full h-362.5 sm:h-380 xl:h-394 flex flex-col gap-4 sm:gap-8 xl:gap-10 mt-13.25 sm:mt-16 xl:mt-18.25 xl:max-w-249 max-h-394 ">
+        <div className="w-full px-4 sm:px-6 bg-[#14151A] pt-6 sm:pt-5 xl:pt-8 flex justify-center min-w-98 min-h-screen max-w-249 m-auto ">
+            <div className="w-full flex flex-col gap-4 sm:gap-8 xl:gap-10 mt-13.25 sm:mt-16 xl:mt-18.25 ">
                 <TabSection />
                 <FilterSeciton />
-                <CardSection />
+                <CardSection cards={mockCards} />
             </div>
         </div>
     );
--- src/components/main/FilterSeciton.tsx
+++ src/components/main/FilterSeciton.tsx
@@ -1,5 +1,79 @@
 'use client';
 
-export default function FilterSeciton() {
-    return <div className="w-full h-10 bg-red-300">필터영역</div>;
+import Dropdown from '../ui/Dropdown';
+import { useState } from 'react';
+
+const regions = [
+    '서울',
+    '경기',
+    '인천',
+    '강원',
+    '충북',
+    '충남',
+    '세종',
+    '대전',
+    '경북',
+    '경남',
+    '대구',
+    '울산',
+    '부산',
+    '전북',
+    '전남',
+    '광주',
+    '제주',
+];
+
+const sortOptions = ['참여인원', '생성일'];
+
+export default function FilterSection() {
+    const [selectedRegion, setSelectedRegion] = useState<string | undefined>();
+    const [selectedSortOption, setSelectedSortOption] = useState<
+        string | undefined
+    >();
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+    const today = new Date();
+    const maxDate = new Date(today.getTime() + 1000 * 60 * 60 * 24 * 30);
+
+    const handleSortSelect = (value: string, order?: 'asc' | 'desc') => {
+        console.log('선택된 정렬:', value, '순서:', order);
+        setSelectedSortOption(value);
+    };
+
+    return (
+        <div className="w-full h-10 flex justify-between">
+            <div className="flex gap-4">
+                <Dropdown
+                    options={regions}
+                    selected={selectedRegion}
+                    onSelect={setSelectedRegion}
+                    placeholder="지역 전체"
+                    iconType="arrow"
+                    showPlaceholderInMenu
+                />
+                <Dropdown
+                    options={[]}
+                    placeholder="날짜 선택"
+                    iconType="date"
+                    selectedDate={selectedDate}
+                    onDateChange={(date) => {
+                        setSelectedDate(date);
+                        console.log('선택된 날짜:', date);
+                    }}
+                    minDate={today}
+                    maxDate={maxDate}
+                />
+            </div>
+            <div>
+                <Dropdown
+                    options={sortOptions}
+                    selected={selectedSortOption}
+                    onSelect={handleSortSelect}
+                    placeholder="마감임박"
+                    iconType="sort"
+                    showPlaceholderInMenu
+                />
+            </div>
+        </div>
+    );
 }
