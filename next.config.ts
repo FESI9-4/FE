292f8ccdf9467a8d32c4d1592a7f2c3b9e571b15
GitHub Oaelan <1djb23@naver.com>
2025-06-09 03:36:11 +0900

    chore: 리액트쿼리 개발툴 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    reactStrictMode: false,
     webpack(config) {
         const fileLoaderRule = config.module.rules.find((rule) =>
             rule.test?.test?.('.svg')
