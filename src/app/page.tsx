0ba0ef9759139ab131652d5780774859a13c5cfb
jaehyeongjung <titochung625@naver.com>
2025-06-09 15:45:08 +0900

    chore: 파일명 오타 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { TabSection, FilterSeciton, CardSection } from '@/components/main';
+import { TabSection, FilterSection, CardSection } from '@/components/main';
 import { mockCards } from '@/__mock__/cards';
 
 //TODO api 연결부 필터링, 카드 데이터, 페이지네이션,
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="w-full px-4 sm:px-6 pt-6 sm:pt-5 xl:pt-8 flex justify-center min-w-98 min-h-screen max-w-249 m-auto ">
             <div className="w-full flex flex-col gap-4 sm:gap-8 xl:gap-10 mt-13.25 sm:mt-16 xl:mt-18.25 ">
                 <TabSection />
-                <FilterSeciton />
+                <FilterSection />
                 <CardSection cards={mockCards} />
             </div>
         </div>
