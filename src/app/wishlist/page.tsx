18a0e6ff0aa21487ac2b00a2f896c07b0d15e284
jaehyeongjung <titochung625@naver.com>
2025-06-11 14:46:40 +0900

    refactor: nav 경로만 수정

--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -5,9 +5,13 @@ export default function WishList() {
     return (
         <div>
             <div className="w-full px-10 h-21 hidden sm:block sm:pt-24 xl:pt-28 max-w-249 m-auto ">
-                <p className='flex flex-col gap-2'>
-                    <span className='text-white text-2xl font-semibold'>찜한 팬팔</span>
-                    <span className='text-sm font-medium text-gray-300'>마감되기 전에 지금 바로 참여해보세요 👀</span>
+                <p className="flex flex-col gap-2">
+                    <span className="text-white text-2xl font-semibold">
+                        찜한 팬팔
+                    </span>
+                    <span className="text-sm font-medium text-gray-300">
+                        마감되기 전에 지금 바로 참여해보세요 👀
+                    </span>
                 </p>
             </div>
             <div className="w-full px-4 sm:px-6 pt-6 sm:pt-25 xl:pt-18 flex justify-center min-w-98 min-h-screen max-w-249 m-auto ">
