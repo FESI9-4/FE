3c0d5ef20e495ac01091c9eae7d4999cb09beaa7
jaehyeongjung <titochung625@naver.com>
2025-06-12 16:58:47 +0900

    refactor: react-qeuries 사용- useBoard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { TabSection, FilterSection, CardSection } from '@/components/main';
-import { CATEGORY_DATA } from '@/types/categories';
-import { boardApi } from '@/utils/apis/boardApi';
-import { Card } from '@/types/card';
+import { useArticles } from '@/hooks/queries/useBoard';
 
 export default function Home() {
     const [activeTab, setActiveTab] = useState<number>(0);
@@ -14,54 +12,24 @@ export default function Home() {
     const [selectedSortOption, setSelectedSortOption] = useState<
         'recent' | 'deadline' | 'person'
     >('recent');
-
     const [sortAsc, setSortAsc] = useState<boolean>(false);
 
-    const [articles, setArticles] = useState<Card[]>([]);
-
-    useEffect(() => {
-        async function fetchArticles() {
-            const bigCategory = CATEGORY_DATA[activeTab].id;
-
-            try {
-                const response = await boardApi.getArticles({
-                    bigCategory,
-                    smallCategory:
-                        selectedCategory === 'ALL' ? '' : selectedCategory,
-                    location: selectedRegion === '전체' ? '' : selectedRegion,
-                    date: selectedDate
-                        ? Math.floor(selectedDate.getTime() / 1000)
-                        : Math.floor(Date.now() / 1000),
-
-                    sort: selectedSortOption,
-                    sortAsc,
-                    lastArticleId: 0,
-                    limit: 10,
-                });
-                setArticles(response.data);
-            } catch (error) {
-                console.error('게시글 불러오기 실패:', error);
-                setArticles([]);
-            }
-        }
-
-        fetchArticles();
-    }, [
+    const { data: articles = [] } = useArticles({
         activeTab,
         selectedCategory,
         selectedRegion,
         selectedDate,
         selectedSortOption,
         sortAsc,
-    ]);
+    });
 
     useEffect(() => {
         setSelectedCategory('ALL');
     }, [activeTab]);
 
     return (
-        <div className="w-full px-4 sm:px-6 pt-6 sm:pt-5 xl:pt-8 flex justify-center min-w-93.75 min-h-screen max-w-249 m-auto ">
-            <div className="w-full flex flex-col gap-4 sm:gap-8 xl:gap-10 mt-13.25 sm:mt-16 xl:mt-18.25 ">
+        <div className="w-full px-4 sm:px-6 pt-6 sm:pt-5 xl:pt-8 flex justify-center min-w-93.75 min-h-screen max-w-249 m-auto">
+            <div className="w-full flex flex-col gap-4 sm:gap-8 xl:gap-10 mt-13.25 sm:mt-16 xl:mt-18.25">
                 <TabSection
                     activeTab={activeTab}
                     setActiveTab={setActiveTab}
