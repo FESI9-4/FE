bfd55d9374a71911aad948ce7f5b92c5f39bb049
jaehyeongjung <titochung625@naver.com>
2025-06-12 15:53:26 +0900

    fix: google new loader 중복 해결 및 최신버전에 따른 코드수정

---
+++ global.d.ts
@@ -0,0 +1,22 @@
+// global.d.ts
+/// <reference types="google.maps" />
+
+declare namespace google.maps.marker {
+  class AdvancedMarkerElement extends google.maps.MVCObject {
+    constructor(options?: google.maps.marker.AdvancedMarkerElementOptions);
+    position: google.maps.LatLng | google.maps.LatLngLiteral;
+    map: google.maps.Map | null;
+    title?: string;
+  }
+
+  interface AdvancedMarkerElementOptions {
+    position?: google.maps.LatLng | google.maps.LatLngLiteral;
+    map?: google.maps.Map | null;
+    title?: string;
+    content?: HTMLElement | string;
+  }
+}
+
+declare namespace google.maps {
+  function importLibrary(name: 'marker'): Promise<typeof google.maps.marker>;
+}
