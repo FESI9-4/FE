caf1261fb22d842ae3f48f4d90dcb78a4ce01533
Accept77 <accept_agreed@kakao.com>
2025-06-18 10:51:59 +0900

    fix: msw type fix

--- src/components/mypage/MyCardListContainer.tsx
+++ src/components/mypage/MyCardListContainer.tsx
@@ -47,6 +47,8 @@ export default function MyCardListContainer() {
     if (isLoading) return <div>Loading...</div>;
     if (isError) return <div>Error</div>;
 
+    console.log('data', data);
+
     return (
         <div>
             {data?.data.length === 0 || !data ? (
