9c7730993d258199be7e825de107d7cfb8076bc0
GitHub Oaelan <1djb23@naver.com>
2025-06-19 15:09:52 +0900

    fix: 모바일 화면에서 컴포넌트 가운데 정렬 되게 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginIllustration, LoginForm } from '@/components/auth';
 
 export default function LoginPage() {
     return (
-        <div className="flex flex-col xl:flex-row xl:gap-[186px] sm:justify-center bg-[#14151A] min-h-screen min-w-screen px-4">
+        <div className="flex flex-col xl:flex-row xl:gap-[186px] justify-center bg-[#14151A] min-h-screen min-w-screen px-4">
             <LoginIllustration />
             <LoginForm />
         </div>
