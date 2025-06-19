e433cc11a206fe8b347ae339221d4ffb7ecaacb8
GitHub Oaelan <1djb23@naver.com>
2025-06-19 15:09:52 +0900

    fix: 모바일 화면에서 컴포넌트 가운데 정렬 되게 수정

--- src/app/(auth)/findpassword/page.tsx
+++ src/app/(auth)/findpassword/page.tsx
@@ -2,7 +2,7 @@ import { LoginIllustration, FindPasswordForm } from '@/components/auth';
 
 export default function FindPasswordPage() {
     return (
-        <div className="flex flex-col xl:flex-row xl:gap-[186px] gap-20 sm:justify-center bg-[#14151A] min-h-screen min-w-screen px-4">
+        <div className="flex flex-col xl:flex-row xl:gap-[186px] gap-20 justify-center bg-[#14151A] min-h-screen min-w-screen px-4">
             <LoginIllustration />
             <FindPasswordForm />
         </div>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import { LoginIllustration, SignupForm } from '@/components/auth';
 
 export default function SignupPage() {
     return (
-        <div className="flex flex-col xl:flex-row xl:gap-[186px] sm:justify-center bg-[#14151A] min-h-screen min-w-screen px-4 sm:py-10">
+        <div className="flex flex-col xl:flex-row xl:gap-[186px] justify-center bg-[#14151A] min-h-screen min-w-screen px-4 sm:py-10">
             <LoginIllustration className="hidden sm:flex" />
             <SignupForm />
         </div>
