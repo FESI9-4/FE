0cdd5d763d22c20fe57a2790cf54eb459e6aaf38
Accept77 <accept_agreed@kakao.com>
2025-06-20 18:51:35 +0900

    fix: tab props fix

--- src/components/ui/Tab.tsx
+++ src/components/ui/Tab.tsx
@@ -1,14 +1,22 @@
 import { cva } from 'class-variance-authority';
 import { ButtonHTMLAttributes } from 'react';
+import { cn } from '@/utils/cn';
 
 interface TabProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
     icon?: React.ReactNode;
     active: boolean;
+    className?: string;
 }
 // TODO 쿼리 파라미터로 새로고침시에도 유지되게 리팩토링
 
-export default function Tab({ children, icon, active, ...props }: TabProps) {
+export default function Tab({
+    children,
+    icon,
+    active,
+    className,
+    onClick,
+}: TabProps) {
     const divClassName = cva(
         'flex items-center gap-1 font-semibold text-lg mx-1.5 justify-center break-keep',
         {
@@ -37,8 +45,11 @@ export default function Tab({ children, icon, active, ...props }: TabProps) {
 
     return (
         <button
-            className="flex flex-col items-center gap-2 hover:cursor-pointer w-full"
-            {...props}
+            className={cn(
+                'flex flex-col items-center gap-2 hover:cursor-pointer w-full',
+                className
+            )}
+            onClick={onClick}
         >
             <div className={divClassName({ active })}>
                 {children}
