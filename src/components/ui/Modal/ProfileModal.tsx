c664f332684127b9f305d038ddb33a693d91129a
Accept77 <accept_agreed@kakao.com>
2025-06-23 14:08:46 +0900

    feat: 프로필 수정, 비밀번호 변경 제외 api 연동

--- src/components/ui/Modal/ProfileModal.tsx
+++ src/components/ui/Modal/ProfileModal.tsx
@@ -9,6 +9,7 @@ import Button from '@/components/ui/Button';
 import Profile from '@/components/ui/Profile';
 import { useGetUser } from '@/hooks/queries/useAuth';
 import InputText from '../InputText';
+import { ProfileEditRequest } from '@/types/myPage';
 
 interface EditProfileModalProps {
     onClose: () => void;
